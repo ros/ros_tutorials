f4c8716e65fae38843a2c4f449f4d48649bfdb30
jfaust <jfaust>
2010-07-29 23:52:20 +0000

    tag talker/listener

--- roscpp_tutorials/listener/listener.cpp
+++ roscpp_tutorials/listener/listener.cpp
@@ -31,11 +31,12 @@
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-
+// %Tag(CALLBACK)%
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
+// %EndTag(CALLBACK)%
 
 int main(int argc, char **argv)
 {
@@ -73,14 +74,18 @@ int main(int argc, char **argv)
    * is the number of messages that will be buffered up before beginning to throw
    * away the oldest ones.
    */
+// %Tag(SUBSCRIBER)%
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+// %EndTag(SUBSCRIBER)%
 
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
    * callbacks will be called from within this thread (the main one).  ros::spin()
    * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
    */
+// %Tag(SPIN)%
   ros::spin();
+// %EndTag(SPIN)%
 
   return 0;
 }
--- roscpp_tutorials/talker/talker.cpp
+++ roscpp_tutorials/talker/talker.cpp
@@ -24,16 +24,20 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
+// %Tag(FULLTEXT)%
+// %Tag(ROS_HEADER)%
 #include "ros/ros.h"
+// %EndTag(ROS_HEADER)%
+// %Tag(MSG_HEADER)%
 #include "std_msgs/String.h"
+// %EndTag(MSG_HEADER)%
 
 #include <sstream>
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
-
+// %Tag(INIT)%
 int main(int argc, char **argv)
 {
   /**
@@ -47,13 +51,16 @@ int main(int argc, char **argv)
    * part of the ROS system.
    */
   ros::init(argc, argv, "talker");
+// %EndTag(INIT)%
 
   /**
    * NodeHandle is the main access point to communications with the ROS system.
    * The first NodeHandle constructed will fully initialize this node, and the last
    * NodeHandle destructed will close down the node.
    */
+// %Tag(NODEHANDLE)%
   ros::NodeHandle n;
+// %EndTag(NODEHANDLE)%
 
   /**
    * The advertise() function is how you tell ROS that you want to
@@ -72,25 +79,34 @@ int main(int argc, char **argv)
    * than we can send them, the number here specifies how many messages to
    * buffer up before throwing some away.
    */
-  ros::Publisher pub = n.advertise<std_msgs::String>("chatter", 1000);
+// %Tag(PUBLISHER)%
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+// %EndTag(PUBLISHER)%
+
+// %Tag(LOOP_RATE)%
+  ros::Rate loop_rate(10);
+// %EndTag(LOOP_RATE)%
 
   /**
    * A count of how many messages we have sent. This is used to create
    * a unique string for each message.
    */
+// %Tag(ROS_OK)%
   int count = 0;
-  ros::Rate r(10);
-  while (n.ok())
+  while (ros::ok())
   {
+// %EndTag(ROS_OK)%
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
+// %Tag(FILL_MESSAGE)%
     std_msgs::String msg;
 
     std::stringstream ss;
     ss << "hello world " << count;
     ROS_INFO("%s", ss.str().c_str());
     msg.data = ss.str();
+// %EndTag(FILL_MESSAGE)%
 
     /**
      * The publish() function is how you send messages. The parameter
@@ -98,14 +114,21 @@ int main(int argc, char **argv)
      * given as a template parameter to the advertise<>() call, as was done
      * in the constructor above.
      */
-    pub.publish(msg);
-    ++count;
+// %Tag(PUBLISH)%
+    chatter_pub.publish(msg);
+// %EndTag(PUBLISH)%
 
+// %Tag(SPINONCE)%
     ros::spinOnce();
-    r.sleep();
+// %EndTag(SPINONCE)%
+
+// %Tag(RATE_SLEEP)%
+    loop_rate.sleep();
+// %EndTag(RATE_SLEEP)%
+    ++count;
   }
 
 
   return 0;
 }
-
+// %EndTag(FULLTEXT)%
