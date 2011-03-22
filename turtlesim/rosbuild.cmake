94ec93c0db137f5ed622d6599d06238979e03ac7
straszheim <troy@industrial-perception.com>
2011-03-22 23:39:16 +0000

    rosbuild2 taking shape.

---
+++ turtlesim/rosbuild.cmake
@@ -0,0 +1,22 @@
+include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
+
+# Set the build type.  Options are:
+#  Coverage       : w/ debug symbols, w/o optimization, w/ code-coverage
+#  Debug          : w/ debug symbols, w/o optimization
+#  Release        : w/o debug symbols, w/ optimization
+#  RelWithDebInfo : w/ debug symbols, w/ optimization
+#  MinSizeRel     : w/o debug symbols, w/ optimization, stripped binaries
+#set(ROS_BUILD_TYPE RelWithDebInfo)
+#set the default path for built executables to the "bin" directory
+#set the default path for built libraries to the "lib" directory
+find_package(wxWidgets REQUIRED)
+include(${wxWidgets_USE_FILE})
+include_directories(${wxWidgets_INCLUDE_DIRS})
+rosbuild_add_boost_directories()
+rosbuild_add_executable(turtlesim_node src/turtlesim.cpp src/turtle.cpp src/turtle_frame.cpp)
+rosbuild_link_boost(turtlesim_node thread)
+target_link_libraries(turtlesim_node ${wxWidgets_LIBRARIES})
+rosbuild_add_executable(turtle_teleop_key tutorials/teleop_turtle_key.cpp)
+rosbuild_add_executable(draw_square tutorials/draw_square.cpp)
+rosbuild_add_executable(mimic tutorials/mimic.cpp)
+
