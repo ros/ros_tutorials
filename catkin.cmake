c091478502b8eb850d92b43f0526003ee66a2561
straszheim <troy@industrial-perception.com>
2011-12-14 01:22:27 +0000

    fix build

--- catkin.cmake
+++ catkin.cmake
@@ -2,7 +2,9 @@ cmake_minimum_required(VERSION 2.8)
 project(ros_tutorials)
 
 find_package(catkin)
-find_package(ROS REQUIRED COMPONENTS roscpp_serialization std_msgs sensor_msgs roscpp rosconsole rostime cpp_common roscpp_traits)
+find_package(ROS REQUIRED COMPONENTS 
+  roscpp_serialization std_msgs sensor_msgs roscpp rosconsole rostime cpp_common roscpp_traits
+  XmlRpc)
 
 add_subdirectory(roscpp_tutorials)
 
