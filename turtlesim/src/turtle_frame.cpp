09b4b4d2a1129f396d4af3350d2838dddfe9aa52
dirk-thomas <dthomas@willowgarage.com>
2012-04-12 20:22:17 +0000

    using fuerte image in turtlesim

--- turtlesim/src/turtle_frame.cpp
+++ turtlesim/src/turtle_frame.cpp
@@ -70,6 +70,7 @@ TurtleFrame::TurtleFrame(QWidget* parent, Qt::WindowFlags f)
   turtles.append("sea-turtle.png");
   turtles.append("diamondback.png");
   turtles.append("electric.png");
+  turtles.append("fuerte.png");
 
   QString images_path = (ros::package::getPath("turtlesim") + "/images/").c_str();
   for (size_t i = 0; i < turtles.size(); ++i)
