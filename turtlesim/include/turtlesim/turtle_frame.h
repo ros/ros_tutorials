65e3e2f9c1d7dcd0086dee70c5e9f58d7ed697f2
William Woodall <wjwwood@gmail.com>
2013-02-26 11:41:53 -0800

    Fix a moc generation error with boost >= 1.48
    See:
    
    https://bugreports.qt-project.org/browse/QTBUG-22829

--- turtlesim/include/turtlesim/turtle_frame.h
+++ turtlesim/include/turtlesim/turtle_frame.h
@@ -34,14 +34,17 @@
 #include <QTimer>
 #include <QVector>
 
-#include <ros/ros.h>
+// This prevents a MOC error with versions of boost >= 1.48
+#ifndef Q_MOC_RUN  // See: https://bugreports.qt-project.org/browse/QTBUG-22829
+# include <ros/ros.h>
 
-#include <std_srvs/Empty.h>
-#include <turtlesim/Spawn.h>
-#include <turtlesim/Kill.h>
-#include <map>
+# include <std_srvs/Empty.h>
+# include <turtlesim/Spawn.h>
+# include <turtlesim/Kill.h>
+# include <map>
 
-#include "turtle.h"
+# include "turtle.h"
+#endif
 
 namespace turtlesim
 {
