565a77f04f4fbc5fb49bca0b429cd15fbae3af46
Gustave Monod <gustave.monod@insa-lyon.fr>
2016-01-18 16:34:18 +0100

    Fixed minor misleading comment
    and changed the double quotes to single for consistency.

--- rospy_tutorials/001_talker_listener/listener.py
+++ rospy_tutorials/001_talker_listener/listener.py
@@ -40,18 +40,18 @@ import rospy
 from std_msgs.msg import String
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
 
 def listener():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
+    # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
-    # name for our 'talker' node so that multiple talkers can
+    # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber('chatter', String, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
