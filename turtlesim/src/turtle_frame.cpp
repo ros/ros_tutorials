474d590104a2f4456c7d42ee6968c20146b13a13
Dirk Thomas <dthomas@osrfoundation.org>
2016-03-16 14:37:52 -0700

    update to Qt5

--- turtlesim/src/turtle_frame.cpp
+++ turtlesim/src/turtle_frame.cpp
@@ -100,7 +100,7 @@ TurtleFrame::TurtleFrame(QWidget* parent, Qt::WindowFlags f)
   spawnTurtle("", width_in_meters_ / 2.0, height_in_meters_ / 2.0, 0);
 
   // spawn all available turtle types
-  if(FALSE)
+  if(false)
   {
     for(int index = 0; index < turtles.size(); ++index)
     {
