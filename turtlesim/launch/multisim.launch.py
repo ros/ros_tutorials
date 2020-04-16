b16e049bcc223f3876cd782041ec72c8372b5526
Jacob Perron <jacob@openrobotics.org>
2020-04-16 16:31:47 -0700

    Replace deprecated launch_ros usage (#84)
    Several Node parameters have been deprecated and replaced with new
    parameters with different names.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- turtlesim/launch/multisim.launch.py
+++ turtlesim/launch/multisim.launch.py
@@ -4,7 +4,7 @@ import launch_ros.actions
 def generate_launch_description():
     return LaunchDescription([
         launch_ros.actions.Node(
-            node_namespace= "turtlesim1", package='turtlesim', node_executable='turtlesim_node', output='screen'),
+            namespace= "turtlesim1", package='turtlesim', executable='turtlesim_node', output='screen'),
         launch_ros.actions.Node(
-            node_namespace= "turtlesim2", package='turtlesim', node_executable='turtlesim_node', output='screen'),
+            namespace= "turtlesim2", package='turtlesim', executable='turtlesim_node', output='screen'),
     ])
