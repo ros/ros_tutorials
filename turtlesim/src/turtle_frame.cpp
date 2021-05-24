fbf42089c8a3db90a1f210362f3ae90fc96c50c2
Chris Lalancette <clalancette@openrobotics.org>
2021-05-24 14:10:52 -0400

    Print out the correct node name on startup. (#122)
    Previously, this code was calling 'get_node_names()' and just
    choosing the first out of the list.  But that could be literally
    anything if there are other nodes running in the network.
    Instead, call 'get_fully_qualified_name()' instead to get the
    name of this node.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- turtlesim/src/turtle_frame.cpp
+++ turtlesim/src/turtle_frame.cpp
@@ -107,7 +107,7 @@ TurtleFrame::TurtleFrame(rclcpp::Node::SharedPtr& node_handle, QWidget* parent, 
   parameter_event_sub_ = nh_->create_subscription<rcl_interfaces::msg::ParameterEvent>(
     "/parameter_events", qos, std::bind(&TurtleFrame::parameterEventCallback, this, std::placeholders::_1));
 
-  RCLCPP_INFO(nh_->get_logger(), "Starting turtlesim with node name %s", nh_->get_node_names()[0].c_str());
+  RCLCPP_INFO(nh_->get_logger(), "Starting turtlesim with node name %s", nh_->get_fully_qualified_name());
 
   width_in_meters_ = (width() - 1) / meter_;
   height_in_meters_ = (height() - 1) / meter_;
