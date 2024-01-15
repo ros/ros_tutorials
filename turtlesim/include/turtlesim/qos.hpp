834acba905e0e17068ca64a8d8f92da808db893f
Yadu <yadunund@gmail.com>
2024-01-15 18:19:00 +0800

    Update turtlesim/include/turtlesim/qos.hpp
    Co-authored-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- turtlesim/include/turtlesim/qos.hpp
+++ turtlesim/include/turtlesim/qos.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2009, Willow Garage, Inc.
+// Copyright (c) 2024, Open Source Robotics Foundation, Inc.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
